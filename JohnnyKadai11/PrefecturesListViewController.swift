57e4ac0dabc7c0fb44eb9add98f00419a5e97f10
0102johnny <rt2824.recruit.1022@gmail.com>
2022-07-05 23:54:50 +0900

    PrefecturesListのTableViewにXIBファイルのセルを表示させる処理の実装

---
+++ JohnnyKadai11/PrefecturesListViewController.swift
@@ -0,0 +1,34 @@
+//
+//  PrefecturesListViewController.swift
+//  JohnnyKadai11
+//
+//  Created by Johnny Toda on 2022/07/05.
+//
+
+import UIKit
+
+class PrefecturesListViewController: UIViewController {
+    @IBOutlet private weak var prefecturesTableView: UITableView!
+
+    private let nib = "PrefecturesTableViewCell"
+    private let cellID = "CellID"
+
+    private let prefecturesData = PrefecturesData().dataArray
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        prefecturesTableView.delegate = self
+        prefecturesTableView.dataSource = self
+        prefecturesTableView.register(UINib(nibName: nib, bundle: nil), forCellReuseIdentifier: cellID)
+    }
+}
+
+extension PrefecturesListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        prefecturesData.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        prefecturesTableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as! PrefecturesTableViewCell
+    }
+}
